ee01f9ee68e7989013919eedc4779bf22804c6dd
Paulina Paluch <paulina.paluch1@pollub.edu.pl>
2020-10-24 19:35:59 +0200

    add web application

---
+++ roomie/src/main/java/com/pm/roomie/RoomieApplication.java
@@ -0,0 +1,13 @@
+package com.pm.roomie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomieApplication.class, args);
+	}
+
+}
